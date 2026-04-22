a424964249e3d2f924d13b36897f84219d0ae305
Randil <randiltharusha72@gmail.com>
2026-04-23 04:50:30 +0530

    feat: Update README and scripts to include module build commands for test clients and server

--- scripts/run-test-clients.sh
+++ scripts/run-test-clients.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
+mvn -DskipTests=true install -pl syos-test-clients -am
 mvn -pl syos-test-clients exec:java -Dexec.mainClass="com.syos.testclient.TestClientRunner"
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
+mvn -DskipTests=true install -pl syos-server -am
 mvn -pl syos-server exec:java -Dexec.mainClass="com.syos.ServerApp"
